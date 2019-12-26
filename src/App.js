edd40aa2a77af1ecb6efb36947fa17fc5b36267b
Acz008 <32407435+miail-t@users.noreply.github.com>
2019-12-26 22:25:33 +0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
-import React, { Component,useState, useEffect } from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
-import rd3 from "react-d3-library";
-import topojson from "topojson";
-import { geoEqualEarth, geoPath } from "d3-geo";
-import { feature } from "topojson-client";
 import World from './World'
 
 
